87f187604d5ec1f1e5a0bd54f4b1140045ddffb6
Juan Tellez <jmtp12poke@gmail.com>
2026-03-30 19:52:21 -0600

    feat(*): add route create profile using prisma

--- src/controllers/profileController.ts
+++ src/controllers/profileController.ts
@@ -1,14 +1,31 @@
 import { type Request, type Response } from "express";
-import prisma from "../database/prismaClient.js";
+import {prisma} from "../../lib/prisma";
 
 export const createProfile = async (req: Request, res: Response) => {
   try {
-    const { userId, bio, avatarUrl } = req.body;
-    const profile = await prisma.profile.create({
+    const { bio, avatarUrl } = req.body;
+    const { userId } = req.user || {};
+    console.log("Creating profile for userId:", userId);
+    if(!userId) {
+        res.status(401).json({ error: "User ID is required" });
+        return;
+    }
+
+    
+    // Verificar si ya existe un perfil para este usuario
+    const existingProfile = await prisma.profiles.findUnique({
+      where: { user_id: userId },
+    });
+
+    if (existingProfile) {
+      return res.status(409).json({ error: 'Profile already exists for this user' });
+    }
+
+    const profile = await prisma.profiles.create({
       data: {
-        userId,
+        user_id: userId,
         bio,
-        avatarUrl,
+        avatar_url: avatarUrl,
       },
     });
     res.status(201).json(profile);
--- src/routes/profileRoutes.ts
+++ src/routes/profileRoutes.ts
@@ -1,8 +1,9 @@
 import { Router } from "express"
 import { createProfile } from "../controllers/profileController.js";
+import { authenticateToken } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
-router.post("/create", createProfile);
+router.post("/create", authenticateToken, createProfile);
 
 export default router;
